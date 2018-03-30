f496c7ecf1a533ef469ece975bb27da01e0f1593
Phil Cable <pcable@wsu.edu>
2018-03-30 15:41:11 -0700

    Provide a hover state for the header logo

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 			</svg>
 			<svg xmlns="http://www.w3.org/2000/svg" width="90" height="20" viewBox="0 0 229 50">
 				<use xlink:href="#logo-inuvo" fill="#404040" />
-				<use xlink:href="#logo-q" fill="#404040" />
+				<use xlink:href="#logo-q" fill="#404040" class="logo-q" />
 			</svg>
 		</a>
 
