21afb06102e660e045655399f868f3937a9a41ac
Phil Cable <pcable@wsu.edu>
2018-04-09 15:17:19 -0700

    Add JS for secondary page header parallax and back to top function

--- js/script.js
+++ js/script.js
@@ -21,19 +21,47 @@
 	let parallax = function() {
 		parallax_elements.forEach( element => {
 			let container = element.getBoundingClientRect();
-			let container_center = container.top + ( container.height / 2 );
-			let viewport_center = window.innerHeight / 2;
-			let y = ( container_center - viewport_center ) / 10;
 
 			if ( window.innerHeight > container.top && 0 < container.bottom ) {
+				let container_center = container.top + ( container.height / 2 );
+				let viewport_center = window.innerHeight / 2;
+				let y = ( container_center - viewport_center ) / 10;
+
 				element.style.backgroundPositionY = "calc(50% + " + y + "px)";
 			}
 		} );
 	};
 
+	document.addEventListener( "DOMContentLoaded", parallax );
+
+	let article_header = document.querySelector( ".article-header" );
+
+	let header_parallax = function() {
+		let container = article_header.getBoundingClientRect();
+
+		if ( 0 < container.bottom ) {
+			let y = window.pageYOffset;
+
+			article_header.style.backgroundPositionY = "calc(50% + " + y + "px)";
+		}
+	};
+
 	window.addEventListener( "scroll", function() {
-		window.requestAnimationFrame( parallax );
+		if ( article_header ) {
+			window.requestAnimationFrame( header_parallax );
+		}
+
+		if ( parallax_elements ) {
+			window.requestAnimationFrame( parallax );
+		}
 	} );
 
-	document.addEventListener( "DOMContentLoaded", parallax );
+	document.querySelector( ".back-to-top a" ).addEventListener( "click", function( e ) {
+		e.preventDefault();
+
+		window.scrollTo( {
+			top: 0,
+			behavior: "smooth"
+		} );
+	} );
 }() );
