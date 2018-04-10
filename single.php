82aa7a49cd7b41b8185f85de85e0eade7c12a880
Phil Cable <pcable@wsu.edu>
2018-04-10 15:53:50 -0700

    Output the publish date for single post views

--- single.php
+++ single.php
@@ -9,7 +9,13 @@
 		</header>
 
 		<div class="article-content">
+
+			<span class="screen-reader-text">Posted on</span>
+
+			<time class="entry-date" datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ); ?>"><?php echo esc_attr( get_the_date() ); ?></time>
+
 			<?php the_content(); ?>
+
 		</div>
 
 	</article>
